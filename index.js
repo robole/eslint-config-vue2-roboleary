c86c8d277bbd17c75c7c6245d5c2c5663ab7a71f
Rob O'Leary <robjoeol@gmail.com>
2021-07-28 15:27:11 +0800

    v0.4.1. fix index.js to use config

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   plugins: ["vue"],
   extends: [
-    "airbnb-base",
+    "eslint-config-roboleary-base",
     "plugin:vue/recommended", // vue 2.x
     "prettier", // makes sure this is last in the list
   ],
@@ -14,14 +14,4 @@ module.exports = {
     ecmaVersion: 2020,
   },
   ignorePatterns: ["**/node_modules/*", "**/dist/*", "**/out/*"],
-  rules: {
-    "no-unused-vars": "warn",
-    "no-console": "off",
-    "func-names": "off",
-    "no-use-before-define": "off",
-    "prefer-const": "off",
-    radix: "off",
-    "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
-    "vue/max-attributes-per-line": 0, //inteferes with prettier
-  },
 };
